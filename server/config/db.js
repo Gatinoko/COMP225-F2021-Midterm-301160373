8ba753c4ef4c3843b58cc41d2c72605445d790d2
Gabriel Tinoco <gabrielslai@gmail.com>
2021-10-25 14:24:58 -0300

    Implemented MongoDB Atlas connection.

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://admin:iesu7LkNSZUoHlaK@mongodbserver.wuso0.mongodb.net/COMP225-F2021-Midterm?retryWrites=true&w=majority"
 };
